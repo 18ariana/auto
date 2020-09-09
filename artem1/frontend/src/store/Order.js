587c258ad22b097a4d2ba60f6c98044488aaa435
18ariana <notwildariadna@gmail.com>
2020-09-09 12:44:37 +0300

    Vuex

--- artem1/frontend/src/store/Order.js
+++ artem1/frontend/src/store/Order.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import Order from "./OrderPrototype";
-import Api from "@/services/api";
+
 
 export default {
   namespaced: true,
@@ -21,14 +21,17 @@ export default {
     currOrder({ commit }, payload) {
       commit('currOrder', payload)
   },
-    orders({ commit}) {
-
+    orders({ commit, dispatch}) {
+       commit('clearError');
+      commit('setLoading', true);
       axios
         .get('http://127.0.0.1:8000/orders/')
         .then((response) => {
           let data = response.data;
+          dispatch('set_orders', data);
 
           commit('set_orders', data);
+          commit('setLoading', false)
         })
     }
   },
